69e272f75ffbf1accb730830a4db0c81fe5ddf11
Vladimir Dabarov <dabarov@gmail.com>
2018-01-04 13:29:49 +0700

    Добавлен модульный тест на FixNum

---
+++ ConsoleApp1/FixNum.cs
@@ -0,0 +1,69 @@
+﻿using System;
+
+public class FixNum
+{
+    private int _value;
+
+    private const int range = 16;
+
+	public FixNum(int value = 0)
+	{
+        _value = value << range;
+	}
+
+    public FixNum(double value)
+    {
+        value *= 1 << range;
+        _value = (int)value;
+    }
+
+    public static FixNum operator +(FixNum a, FixNum b)
+    {
+        FixNum res = new FixNum
+        {
+            _value = a._value + b._value
+        };
+        return res;
+    }
+
+    public static FixNum operator -(FixNum a, FixNum b)
+    {
+        FixNum res = new FixNum
+        {
+            _value = a._value - b._value
+        };
+        return res;
+    }
+
+    public static FixNum operator *(FixNum a, FixNum b)
+    {
+        Int64 newvalue = a._value;
+        newvalue *= b._value;
+        newvalue >>= range;
+        FixNum res = new FixNum
+        {
+            _value = (int)newvalue
+        };
+        return res;
+    }
+
+    public static FixNum operator /(FixNum a, FixNum b)
+    {
+        Int64 newvalue = a._value;
+        newvalue <<= range;
+        newvalue /= b._value;
+        FixNum res = new FixNum
+        {
+            _value = (int)newvalue
+        };
+        return res;
+    }
+
+    public double ToDouble() => (double)_value / (1 << range);
+
+    public override string ToString() => "" + ToDouble();
+
+    public void Print() => Print(this);
+
+    public static void Print(FixNum value) => Console.WriteLine(value.ToDouble());
+}
---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            FixNum fn = new FixNum(15);
+            FixNum.Print(fn);
+            FixNum fn2 = fn / new FixNum(7.0);
+            FixNum.Print(fn2);
+            Console.WriteLine(fn2 * new FixNum(7));
+            Console.WriteLine(fn / fn2);
+            Console.ReadLine();
+        }
+    }
+}
